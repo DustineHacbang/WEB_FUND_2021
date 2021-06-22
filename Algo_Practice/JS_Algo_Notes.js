e06d76be1ab61b6d88c0a10a449e2cc2c48ac213
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-06-22 13:30:57 -0700

    Button_up

---
+++ Algo_Practice/JS_Algo_Notes.js
@@ -0,0 +1,26 @@
+// Variables
+var myName = "Edward Iim" //STRING
+var age ="30" //String
+
+var myAge = 30 //NUMBER OR INTEGERS
+var weight = 190.5 // FLOATS OR DOUBLE
+var sleepy = true //BOOLEAN *make sure its lowercase
+
+//ARRAYS - ORDERED DATAT STRUCTURE
+var names = ['Edward','John','Jane']
+
+console.log (names[4]) //If value is no there it will come in as ERROR or UNDIFINED
+
+// FOR LOOPS AND WHILE LOOPS
+
+//       Step1      Step2    Step3
+for(var num = 1; num < 100; num++){
+    console.log(num)
+}      
+              
+var num = 1
+console.log(num)
+num++ // num = num + 1 or num += 1
+console.log(num) // 2
+num++
+console.log(num) // 3
