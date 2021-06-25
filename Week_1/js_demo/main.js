e95c9955e5becc7e114c787f7b5f4cdcffa08d34
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-06-25 15:01:26 -0700

    profile_page

--- Week_1/js_demo/main.js
+++ Week_1/js_demo/main.js
@@ -6,3 +6,8 @@ function hanndlesClick(){
     alert('the h1 has benn clicked')
 }
 
+function changecolor(){
+    var h2 = document.querySelector("h2")
+    h2.style.backgroundColor = "black"
+    h1.style.color = "white"
+}
