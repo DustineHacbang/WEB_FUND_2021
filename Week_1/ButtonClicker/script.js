f02c506a999bc8b7ff051397a7746f84ebe3ee57
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-06-27 19:15:50 -0700

    counter

--- Week_1/ButtonClicker/script.js
+++ Week_1/ButtonClicker/script.js
@@ -1,8 +1,15 @@
 
-var click = 0
-function handlesClick(element){
-    click++
-    element.innerHTML = `${click} like `
+var click1 = 0
+function handlesClick1(element){
+    click1++
+    element.innerHTML = `${click1} like `
+    confirm('Ninja was liked')
+}
+
+var click2 = 0
+function handlesClick2(element){
+    click2++
+    element.innerHTML = `${click2} like `
     confirm('Ninja was liked')
 }
 
