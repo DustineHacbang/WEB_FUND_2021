70c69a583029e6595f3d3b1146bd2f4a8fc37a8a
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-06-29 07:49:57 -0700

    SingleUserLikeButton

--- Week_2/LikingLikes/SingleUser/script.js
+++ Week_2/LikingLikes/SingleUser/script.js
@@ -1,9 +1,4 @@
 var click = 0
-//  var firstLike = documant.querySelector("likeCounter")
-//  function addLikes(){
-//      likes[0]++;
-//      firstLike.innerText = likes[0] + "like(s)"
-// }
 
 function handlesClick(){
     click++
