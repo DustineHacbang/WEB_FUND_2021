77aef68ced74cb1b15e2e7c45a66b73aaacf9807
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-06-29 09:02:18 -0700

    LikingLike_0

---
+++ Week_2/LikingLikes/MultiUser/script.js
@@ -0,0 +1,20 @@
+var click = 0
+
+function handlesClick(){
+    click++
+    likeCounter.innerHTML = `${click} Like(s) `
+}
+
+var click2 = 0
+
+function handlesClick2(){
+    click2++
+    likeCounter2.innerHTML = `${click2} Like(s) `
+}
+
+var click3 = 0
+
+function handlesClick3(){
+    click3++
+    likeCounter3.innerHTML = `${click3} Like(s) `
+}
