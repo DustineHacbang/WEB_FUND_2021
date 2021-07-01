1ebf0afb3db2650221fbe848b457136204b2ef2f
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-07-01 07:21:48 -0700

    EasyBayWebsite

---
+++ EasyBayWebsite/script.js
@@ -0,0 +1,20 @@
+
+    function handlesClick(){
+        confirm('Your cart is empty')
+    }
+
+    function ChangePic1(){
+        document.getElementById("succulent").src = "images/succulents-1.jpg";
+    }
+    
+    function ChangePic2(){
+        document.getElementById("succulent").src = "images/succulents-2.jpg";
+    }
+
+//     function changePic1(element) {
+//         document.getElementById("succulent").src = "images\assets\succulents-1.jpg";
+// }
+//     function changePic2(element){
+//         document.getElementById("succulent").src = "images\assets\succulents-2.jpg";
+// }
+
